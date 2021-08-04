bd4c83c4628eab4758dc994ad0fa8c3dbcac59cc
Daniel Hernández <danhernan23i@gmail.com>
2021-08-03 22:54:06 -0400

    Add test homepage

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <button @click="show = true">Show modal</button>
+    <ModalWrapper :show="show" @close="show = false" mobile keep-alive>
+      <BaseModal title="title" message="Message" />
+    </ModalWrapper>
+  </div>
 </template>
 
 <script>
+import ModalWrapper from './modal/ModalWrapper.vue';
+import BaseModal from './modal/BaseModal.vue';
+
 export default {
   name: 'App',
+  components: {
+    ModalWrapper,
+    BaseModal,
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
